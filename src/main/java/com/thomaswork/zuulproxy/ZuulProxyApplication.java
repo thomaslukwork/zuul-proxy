8479d6dde82d180a141cc4b6724fdd002fd38f0c
Thomas Luk <wing.chungluk@faber.work>
2020-07-27 17:17:27 +0100

    first commit

---
+++ src/main/java/com/thomaswork/zuulproxy/ZuulProxyApplication.java
@@ -0,0 +1,21 @@
+package com.thomaswork.zuulproxy;
+
+import com.thomaswork.zuulproxy.filter.SimpleFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZuulProxyApplication.class, args);
+  }
+
+  @Bean
+  public SimpleFilter simpleFilter() {
+    return new SimpleFilter();
+  }
+}
