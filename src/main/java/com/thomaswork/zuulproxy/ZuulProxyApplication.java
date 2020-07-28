70abc230ad617859a2588c8afba0d67d3d684baa
Thomas Luk <thomasluk@gmail.com>
2020-07-28 22:50:54 +0100

    add configuration that can work with eureka

--- src/main/java/com/thomaswork/zuulproxy/ZuulProxyApplication.java
+++ src/main/java/com/thomaswork/zuulproxy/ZuulProxyApplication.java
@@ -3,12 +3,13 @@ package com.thomaswork.zuulproxy;
 import com.thomaswork.zuulproxy.filter.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ZuulProxyApplication {
 
   public static void main(String[] args) {
